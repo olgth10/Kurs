d2e9cf8ca6a7ddf5b4291083f74208b9cb9fa140
olgth10@github.com <olgth10@github.com>
2016-05-05 01:33:49 +0300

    Revert "bands modified"
    This reverts commit d0ab9044b8fce4db17be6f5fdf9ab9c9e950c108.

--- Models/Band.cs
+++ Models/Band.cs
@@ -16,6 +16,49 @@ namespace Kurs.Models
         {
             name = n;
             ls = new List<Criminal>();
-        }        
+        }
+
+        #region Добавление банды
+        public void Add()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(@"../../Data/Bands.xml");
+            XmlNode root = doc.DocumentElement;
+            XmlElement band = doc.CreateElement("band");
+            XmlElement tag = doc.CreateElement("name");            
+            tag.InnerText = name;
+            band.AppendChild(tag);
+            doc.Save(@"../../Data/Bands.xml");
+        }
+        #endregion
+
+        #region Добавление преступников в банду
+        public void AddInBand()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(@"../../Data/Bands.xml");
+            XmlNode root = doc.DocumentElement;
+            XmlElement band = doc.CreateElement("band");
+            XmlElement tag = doc.CreateElement("name");
+            tag.InnerText = name;
+            band.AppendChild(tag);
+            doc.Save(@"../../Data/Bands.xml");
+        }
+        #endregion
+
+        #region Изменение банды
+        public void Edit()
+        {
+
+        }
+        #endregion
+
+        #region Удаление банды
+        public void Delete()
+        {
+
+        }
+        #endregion
+        
     }
 }
